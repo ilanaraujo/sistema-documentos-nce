a410a01598cffcfe667fda36fede81bd757f1810
Lucas Jara <53538223+JaraLucas@users.noreply.github.com>
2020-08-05 19:03:47 -0300

    Update email.py

--- email.py
+++ email.py
@@ -15,22 +15,22 @@ app.config['MAIL_DEFAULT_SENDER'] = None
 app.config['MAIL_MAX_EMAILS'] = None 
 app.config['MAIL_ASCII_ATTACHEMENTS'] = False
 #função de envio de email para usuário ativo
-def mail_ativo(id)
+def mail_ativo(id):
         msg = Message('Ativação no sistema', recipients= [usuarioInativo.email])
-        msg.body = 'Olá sr/sra. %s, seu cadastro com o email:%s  foi ativado e está pronto para uso no sistema!\n Atenciosamente, coordenação NCE'%(usuarioInativo.nome,usuarioInativo.email)
+        msg.body = ('Olá sr/sra. {}, seu cadastro com o email: {}  foi ativado e está pronto para uso no sistema!\n Atenciosamente, coordenação NCE'.format(usuarioInativo.nome,usuarioInativo.email))
         mail.send(msg)
 #função de envio de email para usuário inativo
 def mail_inativo(id):
         msg = Message('Desativação  no sistema', recipients= [usuarioAtivo.email])
-        msg.body = 'Olá sr/sra. %s, seu cadastro com o email: %s  foi desativado do sistema!\n Atenciosamente, coordenação NCE'%(usuarioAtivo.nome,usuarioAtivo.email)
+        msg.body = ('Olá sr/sra. {}, seu cadastro com o email: {}  foi desativado do sistema!\n Atenciosamente, coordenação NCE'.format(usuarioAtivo.nome,usuarioAtivo.email))
         mail.send(msg)
 #função de envio de email para usuário aprovado
 def mail_aprovado(id):
         msg = Message('Aprovação do cadastro', recipients= [usuarioNovo.email])
-        msg.body = 'Olá sr/sra. %s, seu cadastro com o email:%s  foi aprovado e está pronto para uso no sistema!\n Atenciosamente, coordenação NCE'%(usuarioNovo.nome,usuarioNovo.email)'
+        msg.body = ('Olá sr/sra. {}, seu cadastro com o email:{}  foi aprovado e está pronto para uso no sistema!\n Atenciosamente, coordenação NCE'.format(usuarioNovo.nome,usuarioNovo.email))
         mail.send(msg)
 #função de envio de email para usuário reprovado
-def mail_reprovado(id):)'
+def mail_reprovado(id):
         msg = mail('Cadastro reprovado.', recipients=[usuarioNovo.email])
-        msg.body = ('Seu cadastro foi reprovado pelos seguintes erros:\n %s' %{{form.cadastroReprovado}})
+        msg.body = ('Seu cadastro foi reprovado pelos seguintes erros:\n {}'.format({{form.cadastroReprovado}})
         mail.send(msg)
